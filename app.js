e429c18df95337e60e647d770013782c70f2c603
Satit Rianpit <siteslave@msn.com>
2015-04-04 13:17:13 +0700

    add login

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,8 +24,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(session({
+    secret: 'SecretKey))))',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+var auth = function (req, res, next) {
+    if (req.session.username) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+app.use('/users', auth, users);
+app.use('/login', login);
+
+app.use('/', auth, routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,14 @@ var router = express.Router();
 /**
  * GET /users
  */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('users/index');
 });
 
 /**
  * GET /users/new
  */
-router.get('/new', function(req, res, next) {
+router.get('/new', function(req, res) {
   res.render('users/new');
 });
 
