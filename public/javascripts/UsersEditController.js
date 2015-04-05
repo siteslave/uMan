2cca77e0bb2ee10101a26d375f01010f3b52ca20
Satit Rianpit <siteslave@msn.com>
2015-04-05 13:54:10 +0700

    add update

--- public/javascripts/UsersEditController.js
+++ public/javascripts/UsersEditController.js
@@ -1,3 +1,27 @@
-App.controller('UsersEditController', function ($scope) {
+App.controller('UsersEditController', function ($scope, $routeParams, $location, UsersEditService) {
+
+    $scope.id = $routeParams.id;
+
+    UsersEditService.edit($scope.id)
+        .then(function (data) {
+
+            $scope.username = data.users.username;
+            $scope.fullname = data.users.fullname;
+            $scope.active = data.users.active == 'Y' ? true : false;
+        }, function (err) {
+            console.log(err);
+        });
+
+
+    $scope.save = function () {
+        var active = $scope.active ? 'Y' : 'N';
+
+        UsersEditService.saveEdit($scope.id, $scope.fullname, active)
+            .then(function () {
+                $location.path('/');
+            }, function (err) {
+                console.log(err);
+            });
+    };
 
 });
