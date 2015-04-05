7ff230d3030f315e39f30c49cd2f98c0320ab723
Satit Rianpit <siteslave@msn.com>
2015-04-05 15:53:56 +0700

    add alert upload

--- public/javascripts/UploadController.js
+++ public/javascripts/UploadController.js
@@ -1,10 +1,11 @@
-App.controller('UploadController', function ($scope, LxProgressService) {
+App.controller('UploadController', function ($scope, LxProgressService, LxNotificationService) {
     $scope.submit = function () {
         LxProgressService.circular.show('#5fa2db', '#progress');
         return true;
     };
 
     $scope.complete = function(content) {
+        LxNotificationService.success('Upload file success');
         LxProgressService.circular.hide();
       console.log(content); // process content
     }
